eb50201ded348584947bfa5ea928dcee5365a3a9
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-04-29 12:47:03 -0300

    Adicionado classe para Converter FlowDocument para tipo string

---
+++ Converter/FlowDocumentToString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace PersonalNotesApp.Converter
+{
+	public class FlowDocumentToString
+	{
+		public static string Converte(FlowDocument documento)
+		{
+			return new TextRange(documento.ContentStart, documento.ContentEnd).Text;
+		}
+
+		public static FlowDocument ConverteDeVolta(string texto)
+		{
+			FlowDocument documento = new FlowDocument();
+			documento.Blocks.Add(new Paragraph(new Run(texto)));
+			return documento;
+		}
+	}
+}
