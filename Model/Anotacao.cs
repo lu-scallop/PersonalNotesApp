28fa7b869b45eb8a268faf5664fc541aca4e19b8
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-04-29 12:50:24 -0300

    Modificado classe Anotacao

--- Model/Anotacao.cs
+++ Model/Anotacao.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace PersonalNotesApp.Model
 {
     public class Anotacao : Base
     {
-        private string _texto;
-        public string Texto 
+        private FlowDocument _conteudo;
+        public FlowDocument Conteudo 
         {
-            get => _texto;
+            get => _conteudo;
             set
             {
-                _texto = value;
-                OnPropertyChanged(nameof(Texto));
+                _conteudo = value;
+                OnPropertyChanged(nameof(Conteudo));
             }
         }
         public Anotacao(string nome)
         {
             Nome = nome;
-            Texto = "Olá Mundo!";
+            Conteudo = new FlowDocument(new Paragraph(new Run("Olá Mundo")));
         }
     }
 }
