28777646d66a9490bb294332c30b891dd06a2b02
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-05-22 19:08:24 -0300

    Atualizado métodoe Converter

--- Converter/FlowDocumentToString.cs
+++ Converter/FlowDocumentToString.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Documents;
 
 namespace PersonalNotesApp.Converter
@@ -11,7 +12,35 @@ namespace PersonalNotesApp.Converter
 	{
 		public static string Converte(FlowDocument documento)
 		{
-			return new TextRange(documento.ContentStart, documento.ContentEnd).Text;
+			//return new TextRange(documento.ContentStart, documento.ContentEnd).Text;
+
+			var markdownBuilder = new StringBuilder();
+
+			foreach (var block in documento.Blocks)
+			{
+                if (block is Paragraph paragraph)
+                {
+                    foreach (var inline in paragraph.Inlines)
+                    {
+                        if (inline is Run run)
+                        {
+							string text = run.Text;
+							if (run.FontWeight == FontWeights.Bold) text = $"**{text}**";
+							if (run.FontStyle == FontStyles.Italic) text = $"*{text}*";
+							if (run.TextDecorations.Contains(TextDecorations.Underline[0])) text = $"<u>text</u>";
+
+							markdownBuilder.Append(text);
+						}
+						else if(inline is LineBreak)
+						{
+							markdownBuilder.AppendLine();
+						}
+                    }
+                }
+				markdownBuilder.AppendLine();
+            }
+
+			return markdownBuilder.ToString();
 		}
 
 		public static FlowDocument ConverteDeVolta(string texto)
