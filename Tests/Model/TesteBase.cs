55420281277498fe02bc2c655ca52af50200a905
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-06-04 23:55:34 -0300

    Adicionado pasta Tests onde serão introduzidos testes cruciais para o programa

---
+++ Tests/Model/TesteBase.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonalNotesApp.Tests.Model
+{
+	[TestFixture]
+	internal class TesteBase
+	{
+		[Test]
+		public void Nome_QuandoAtribuido_AtivaPropertyChanged()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+
+		}
+
+	}
+}
