4ad69b64debbe81227d82c6a8a7f5c820dd898d7
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-06-10 23:37:51 -0300

    Estrutura do projeto reorganizada

---
+++ PersonalNotesApp.Tests/Model/AnotacaoTest.cs
@@ -0,0 +1,29 @@
+﻿using PersonalNotesApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using Xunit;
+
+namespace PersonalNotesApp.Tests.Model
+{
+	public class AnotacaoTest
+	{
+		[Fact]
+		public void Construtor_DeveInicializarPropriedadesCorretamente()
+		{
+			//Arrange
+			string nomeEsperado = "Meu primeiro teste";
+
+			//Act
+			var anotacao = new Anotacao(nomeEsperado);
+
+			//Assert
+			Assert.Equal(nomeEsperado, anotacao.Nome);
+			Assert.NotNull(anotacao.Conteudo);
+			Assert.IsType<FlowDocument>(anotacao.Conteudo);	
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal Notes App
+![Bagde](https://img.shields.io/badge/status-completo...-green)
+
+Um aplicativo Desktop de anotação. Estou utilizando o framework WPF para desenvolvê-lo.
