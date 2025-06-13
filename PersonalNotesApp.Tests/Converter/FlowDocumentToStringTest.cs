6babb2206ec5d8ae5b243ab8cdceff24e51118bc
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-06-13 12:38:29 -0300

    Feito primeiro teste para o método Converte da classe FlowDocumentToString

---
+++ PersonalNotesApp.Tests/Converter/FlowDocumentToStringTest.cs
@@ -0,0 +1,29 @@
+﻿using PersonalNotesApp.Converter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace PersonalNotesApp.Tests.Converter
+{
+	public class FlowDocumentToStringTest
+	{
+		[Fact]
+		public void Converte_FlowDocumentParaString_RetornaString()
+		{
+			//Arrange
+			var flowDocument = new FlowDocument(new Paragraph(new Run("Isto é um teste!")));
+
+			//Act
+			var textoConvertido = FlowDocumentToString.Converte(flowDocument);
+
+			//Assert
+			Assert.NotNull(textoConvertido);
+			Assert.IsType<string>(textoConvertido);
+			Assert.Contains("Isso é um teste!", textoConvertido);
+			
+		}
+	}
+}
