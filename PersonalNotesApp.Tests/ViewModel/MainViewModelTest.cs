143ef85005d214143052c0625c6e153eeff120ac
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-06-23 12:49:36 -0300

    Adicionado testes para o método ExcluirItem

--- PersonalNotesApp.Tests/ViewModel/MainViewModelTest.cs
+++ PersonalNotesApp.Tests/ViewModel/MainViewModelTest.cs
@@ -1,4 +1,5 @@
 ﻿using PersonalNotesApp.Model;
+using PersonalNotesApp.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -49,7 +50,7 @@ namespace PersonalNotesApp.Tests.ViewModel
 		}
 
 		[Fact]
-		public void AdicionaSubPasta_AdicionaSubPasta_DeveSerAdicionadoNaColecao()
+		public void AdicionaSubPasta_SubPastaAdicionadaNaColecao_DeveSerAdicionadoNaColecao()
 		{
 			Pasta pasta = new Pasta("Pasta principal");
 			string nomeSubPasta = "Nova Sub Pasta";
@@ -61,6 +62,95 @@ namespace PersonalNotesApp.Tests.ViewModel
 
 		}
 
+		[Fact]
+		public void AdicionaNovaAnotacao_AnotacaoAdicionadaNaColecao_AnotacaoDeveEstarNaColecao()
+		{
+			ObservableCollection<Base> colecao = new ObservableCollection<Base>();
+			Anotacao anotacaoTeste = new Anotacao("Nova anotacao");
+
+			colecao.Add(anotacaoTeste);
+
+			Assert.True(colecao.Contains(anotacaoTeste));
+		}
+
+		[Fact]
+		public void AdicionaNovaAnotacao_NomeAnotacaoNulaAdicionadaNaColecao_LancaExcecao()
+		{
+
+			string nomeAnotacao = null;
+
+			Assert.Throws<ArgumentNullException>(() => new Anotacao(nomeAnotacao));
+		}
+
+		[Fact]
+		public void AdicionaNovaAnotacaoEmSubPasta_AnotacaoAdicionadaNaSubPasta_AnotacaoDeveEstarNaColecao()
+		{
+			Pasta pasta = new Pasta("Pasta principal");
+			Anotacao anotacao = new Anotacao("Nova Anotação");
+
+			pasta.SubPastas.Add(anotacao);
+
+			Assert.True(pasta.SubPastas.Contains(anotacao));
+		}
+
+		[Fact]
+		public void ExcluirItem_ExcluiPastaNaColecao_RemovePasta()
+		{
+			MainViewModel mainViewModel = new MainViewModel();
+			Pasta pasta = new Pasta("Pasta");
+
+			mainViewModel.Pastas.Add(pasta);
+			mainViewModel.ExcluirItem(pasta);
+
+			Assert.Empty(mainViewModel.Pastas);
+			Assert.DoesNotContain(pasta, mainViewModel.Pastas);
+		}
+
+		[Fact]
+		public void ExcluirItem_ExcluiSubPasta_RemoveSubPasta()
+		{
+			MainViewModel mainViewModel = new MainViewModel();
+			Pasta pastaPrincipal = new Pasta("Pasta 1");
+			Pasta subPasta = new Pasta("Pasta 1.1");
+
+			mainViewModel.Pastas.Add(pastaPrincipal);
+			pastaPrincipal.SubPastas.Add(subPasta);
+
+			mainViewModel.ExcluirItem(subPasta);
+
+			Assert.False(pastaPrincipal.SubPastas.Contains(subPasta));
+			Assert.Empty(pastaPrincipal.SubPastas);
+			Assert.True(mainViewModel.Pastas.Contains(pastaPrincipal));
+
+		}
+
+		[Fact]
+		public void ExcluirItem_ExcluirSubPastaEmHierarquiaProfunda_RemoveSubPasta()
+		{
+			MainViewModel mainViewModel = new MainViewModel();	
+			Pasta pastaPrincipal = new Pasta("Pasta 1");
+			var nivel1 = new Pasta("Pasta 1.1");
+			var nivel2 = new Pasta("Pasta 1.2");
+			var nivel3 = new Pasta("Pasta 1.3");
+			var nivel4 = new Pasta("Pasta 1.4");
+
+			mainViewModel.Pastas.Add(pastaPrincipal);
+			pastaPrincipal.SubPastas.Add(nivel1);
+			nivel1.SubPastas.Add(nivel2);
+			nivel2.SubPastas.Add(nivel3);
+			nivel3.SubPastas.Add(nivel4);
+
+			mainViewModel.ExcluirItem(nivel4);
+
+			Assert.False(nivel3.SubPastas.Contains(nivel4));
+			Assert.Empty(nivel3.SubPastas);
+
+			Assert.True(mainViewModel.Pastas.Contains(pastaPrincipal));
+			Assert.True(pastaPrincipal.SubPastas.Contains(nivel1));
+			Assert.True(nivel1.SubPastas.Contains(nivel2));
+			Assert.True(nivel2.SubPastas.Contains(nivel3));
+
+		}
 
 	}
 }
